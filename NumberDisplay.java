df6ac82896a4a965396f1e17eaf6f54e3ad3427c
davidrdglopez <davidrdgzlopez@gmail.com>
2014-11-21 13:20:31 +0100

    Creada la cabecera de la clase NumberDisplay

---
+++ NumberDisplay.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class NumberDisplay here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class NumberDisplay
+{
+    // 
+    private int limit;
+    // Valor almacenado
+    private int value;
+
+    /**
+     * Constructor for objects of class NumberDisplay
+     */
+    public NumberDisplay(int theFuckingLimit)
+    {
+        // initialise instance variables
+        limit = theFuckingLimit;
+        value = 0;
+    }
+}
