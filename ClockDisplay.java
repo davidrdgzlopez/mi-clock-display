13772a354b94b139a251d7db7b9d1c63db7b9433
davidrdglopez <davidrdgzlopez@gmail.com>
2014-11-21 13:33:37 +0100

    Añadido constructor a la clase ClockDisplay

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -7,5 +7,17 @@
  */
 public class ClockDisplay
 {
+        // Objeto NumberDisplay que nos guarda la hora
+    private NumberDisplay hours;
+    // Objeto NUmberDisplay que nos guarda los minutos
+    private NumberDisplay minutes;
+    // String de 5 caracteres: hora, dos puntos y minutos
+    private String displayString;
     
+    //Constructor for objects of class ClockDisplay which set up the time to 00:00
+    public ClockDisplay()
+    {
+        hours = new NumberDisplay (24);
+        minutes = new NumberDisplay (60);
+    }
 }
